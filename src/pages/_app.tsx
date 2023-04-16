58a7ca9f606e453bcb9505cb2b47b253ef308d94
Farzin <91827310+FarzinMoha@users.noreply.github.com>
2023-04-16 22:19:17 +0300

    add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto_Slab } from "next/font/google";
+const Roboto = Roboto_Slab({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={Roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
